c2504fd27573189316099bc5d5644a0cef58065a
Fabricio C Zuardi <fabricio@fabricio.org>
2016-05-12 01:29:47 -0300

    include support for object spread (experimental)

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ module.exports = {
         indent: [2, 4],
         'comma-dangle': [2, 'never'],
         'no-console': 0
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true
+        }
     }
 };
